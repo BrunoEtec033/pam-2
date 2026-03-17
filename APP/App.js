bb19e25fa3553874c147767d044631ea88bebcc7
ELiton Camargo <camargoliveira@gmail.com>
2026-03-17 12:03:15 -0300

    Final Aula-04

--- APP/App.js
+++ APP/App.js
@@ -4,3 +4,19 @@ import AppRoutes from "./routes/app.routes";
 export default function App() {
   return <AppRoutes />;
 }
+
+// Usuário toca na tela
+//         ↓
+// Screen (MovieList)
+//         ↓
+// Hook (useMovies)
+//         ↓
+// Service (movieService)
+//         ↓
+// API (api.js / servidor)
+//         ↓
+// Service retorna dados
+//         ↓
+// Hook atualiza estado
+//         ↓
+// Screen renderiza componentes
